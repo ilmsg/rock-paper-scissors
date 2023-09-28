1fa94c062bbc0ede315eec4fe13396902cc7b04b
eak netpanya <eak.netpanya@gmail.com>
2023-09-28 16:20:18 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+import rock from './rock.png';
+import scissors from './scissors.png';
+import paper from './paper.png';
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src={rock} width={300} height={400} roundedCircle />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src={scissors} width={300} height={400} roundedCircle />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src={paper} width={300} height={400} roundedCircle />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
